ae2b92a0d057d0402fbb322b6d32d9c38bd19428
Juliana <juliana.novais@icloud.com>
2022-06-04 20:30:41 -0300

    Refatorando Jogo e estilos

--- script.js
+++ script.js
@@ -1,92 +1,72 @@
-var pontuacaoJogador = 0;
-var pontuacaoMaquina = 0;
-
-function atualizaContadorAdver(){
-    pontuacaoMaquina++;
-    var elemento = document.getElementById("contAdver");
-    elemento.innerHTML = pontuacaoMaquina
-}
-
-function voceVenceu(){
-    alert('Você venceu!')
-}
-
-function maquinaVenceu(){
-    alert('A máquina venceu!')
-}
-
-function empate(){
-    alert('Empate')
-}
-
-var emJogo = false;
-
-var jogar = prompt('Vamos jogar? 1-SIM / 2 - NÃO')
-
-if (jogar == 1) {
-    emJogo = true;
-} else {
-    emJogo = false;
-}
-
-
-while (emJogo) {
-    
-    alert("Bem-vindo ao jogo!")
-    var escolhaJogador = parseInt(prompt("Escolha entre: 1- Pedra, 2- Papel, 3- Tesoura"));
-    var escolhaMaquina = Math.floor(Math.random() * (3 - 1 + 1) + 1)
-
-    switch(escolhaJogador){
-        case 1:
-            if(escolhaMaquina == 2){
-                atualizaContadorAdver();
-                maquinaVenceu()
-                //atualizaContadorAdver();
-            } else if(escolhaMaquina == 3){
-                pontuacaoJogador ++;
-                voceVenceu()
-            } else{
-                empate()
-            }
-            break;
-        case 2:
-            if(escolhaMaquina ==3){
-                
-                atualizaContadorAdver()
-                maquinaVenceu()
-               //atualizaContadorAdver();
-            } else if(escolhaMaquina ==1){
-                pontuacaoJogador ++;
-               voceVenceu()
-            } else{
-                empate()
-            }
-            break;
-        case 3:
-            if(escolhaMaquina ==1){
-                atualizaContadorAdver()
-                maquinaVenceu()
-                //atualizaContadorAdver();
-            } else if(escolhaMaquina ==2){
-                pontuacaoJogador ++;
-                voceVenceu()
-            } else{
-                empate()
-            }
-            break;
-    
-            }    
-
-    if(pontuacaoJogador ==2){
-        alert('Parabéns, você ganhou o jogo!')
-        emJogo = false
-    } else if(pontuacaoMaquina ==2){
+function iniciarJogo(emJogo) {
+    let jogando = emJogo
+    let pontuacaoJogador = 0
+    let pontuacaoMaquina = 0
+  
+    function atualizarContador(ganhador) {  
+      let contJogador = document.getElementById('contJogador')
+      let contAdver = document.getElementById('contAdver')
+      
+      if (ganhador == contJogador) {
+        pontuacaoJogador = pontuacaoJogador + 1
+        alert('Parabéns, você ganhou a rodada!')
+      } else if (ganhador == contAdver) {
+        pontuacaoMaquina = pontuacaoMaquina + 1
+        alert('A máquina ganhou a rodada!')
+      }
+  
+      contJogador.innerHTML = pontuacaoJogador;
+      contAdver.innerHTML = pontuacaoMaquina;
+  
+      // verifica ganhador do jogo
+      if (pontuacaoJogador >= 2) {
+        alert('Você ganhou o jogo!')
+        jogando = false    
+      }
+      if (pontuacaoMaquina >= 2) {
         alert('A máquina ganhou o jogo!')
-        emJogo = false
-    } else {
-    emJogo = true
+        jogando = false      
+      } 
     }
-   
-    console.log("Escolha da Máquina:", escolhaMaquina)
-}
-
+  
+    while (jogando) {
+      let escolhaJogador = parseInt(prompt('Escolha: 1-Pedra, 2-Papel, 3-Tesoura'))
+  
+      if (escolhaJogador == 1 || escolhaJogador == 2 || escolhaJogador == 3) {
+        let escolhaMaquina = Math.floor(Math.random() * (3 - 1 + 1) + 1)  
+        switch (escolhaJogador) {
+          case 1:
+            if (escolhaMaquina == 2) {
+              atualizarContador(contAdver)
+            } else if (escolhaMaquina == 3) {
+              atualizarContador(contJogador)
+            } else {
+              alert('Empate!')
+            }
+            break
+          case 2:
+            if (escolhaMaquina == 3) {
+              atualizarContador(contAdver)
+            } else if (escolhaMaquina == 1) {
+              atualizarContador(contJogador)
+            } else {
+              alert('Empate!')
+            }
+            break
+          case 3:
+            if (escolhaMaquina == 1) {
+              atualizarContador(contAdver)
+            } else if (escolhaMaquina == 2) {
+              atualizarContador(contJogador)
+            } else {
+              alert('Empate!')
+            }
+            break
+          }
+        
+      } else {
+        alert('Opção inválida, tente novamente!')
+      }
+    }
+  }
+  
